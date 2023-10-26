c54976b4edcc34221177949097122a4cd64424df
Kian Rafael Lagado <134679503+kianlagado@users.noreply.github.com>
2023-10-26 17:00:02 +0800

    Update FilterSortActivity.java

--- app/src/main/java/com/mobdeve/s16/lagado/kian/mco2_abelgas_lagado_llamado/FilterSortActivity.java
+++ app/src/main/java/com/mobdeve/s16/lagado/kian/mco2_abelgas_lagado_llamado/FilterSortActivity.java
@@ -1,8 +1,11 @@
 package com.mobdeve.s16.lagado.kian.mco2_abelgas_lagado_llamado;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class FilterSortActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class FilterSortActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter_sort);
+
+        // Populating the Spinner
+        Spinner genreSpinner = findViewById(R.id.genre_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.genres_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        genreSpinner.setAdapter(adapter);
+
+        // Close Button functionality
+        ImageButton closeButton = findViewById(R.id.close_button);
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
