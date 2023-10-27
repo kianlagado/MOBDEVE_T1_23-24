c27f1c18d33888adc41081a96f376581f905acd3
Kian Rafael Lagado <134679503+kianlagado@users.noreply.github.com>
2023-10-27 17:17:53 +0800

    Update DetailActivity.java

--- app/src/main/java/com/mobdeve/s16/lagado/kian/mco2_abelgas_lagado_llamado/DetailActivity.java
+++ app/src/main/java/com/mobdeve/s16/lagado/kian/mco2_abelgas_lagado_llamado/DetailActivity.java
@@ -2,7 +2,10 @@ package com.mobdeve.s16.lagado.kian.mco2_abelgas_lagado_llamado;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        ImageButton backButton = findViewById(R.id.btn_exit_detail);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        /*
+        Intent intent = getIntent();
+        if (intent != null) {
+            Anime receivedAnime = intent.getParcelableExtra("SELECTED_ANIME");
+            // Now you can use 'receivedAnime' to populate your views in DetailActivity.
+        }*/
     }
 }
