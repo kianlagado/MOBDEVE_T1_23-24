43f56e7bff12d42510baaa95a6601eba1f88a6b2
Kian Rafael Lagado <134679503+kianlagado@users.noreply.github.com>
2023-12-02 19:10:10 +0800

    Update AnimeAdapter.java

--- app/src/main/java/com/mobdeve/s16/lagado/kian/mco2_abelgas_lagado_llamado/AnimeAdapter.java
+++ app/src/main/java/com/mobdeve/s16/lagado/kian/mco2_abelgas_lagado_llamado/AnimeAdapter.java
@@ -10,6 +10,8 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.squareup.picasso.Picasso; // Ensure Picasso library is included in your project
+
 import java.util.List;
 
 public class AnimeAdapter extends RecyclerView.Adapter<AnimeAdapter.AnimeViewHolder> {
@@ -17,14 +19,14 @@ public class AnimeAdapter extends RecyclerView.Adapter<AnimeAdapter.AnimeViewHol
     private List<TestAnime> animeList;
     private Context context;
 
-    private OnItemClickListener Listener;
+    private OnItemClickListener listener;
 
     public interface OnItemClickListener {
         void onItemClick(int position);
     }
 
     public void setOnItemClickListener(OnItemClickListener listener) {
-        Listener = listener;
+        this.listener = listener;
     }
 
     // Constructor
@@ -52,36 +54,34 @@ public class AnimeAdapter extends RecyclerView.Adapter<AnimeAdapter.AnimeViewHol
             synopsis = itemView.findViewById(R.id.synopsis);
             rating = itemView.findViewById(R.id.rating);
 
-            itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (listener != null) {
-                        int position = getAdapterPosition();
-                        if (position != RecyclerView.NO_POSITION) {
-                            listener.onItemClick(position);
-                        }
+            itemView.setOnClickListener(v -> {
+                if (listener != null) {
+                    int position = getAdapterPosition();
+                    if (position != RecyclerView.NO_POSITION) {
+                        listener.onItemClick(position);
                     }
                 }
             });
-
         }
-
     }
 
     @NonNull
     @Override
     public AnimeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
-        return new AnimeViewHolder(view, Listener);
+        return new AnimeViewHolder(view, listener);
     }
 
     @Override
     public void onBindViewHolder(@NonNull AnimeViewHolder holder, int position) {
         TestAnime anime = animeList.get(position);
-        holder.thumbnail.setImageResource(anime.getThumbnail());
+
+        // Use Picasso to load the image from the URL
+        Picasso.get().load(anime.getImageUrl()).into(holder.thumbnail);
+
         holder.title.setText(anime.getTitle());
         holder.synopsis.setText(anime.getSynopsis());
-        holder.rating.setText(anime.getRating());
+        holder.rating.setText(String.valueOf(anime.getScore())); // Assuming score is a double
     }
 
     @Override
